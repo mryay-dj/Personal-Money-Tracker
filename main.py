aecfe81c4fd6bace1cad510443da8c0653601b7b
mryay-dj <2424djones@gmail.com>
2026-09-19 13:12:59 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+menu = int(input("Welcome to Your Personal Finance Tracker \n" \
+"1. Add Income \n" \
+"2. Add expense \n" \
+"3. View Balance \n" \
+"4. Exit \n"))
+print("")
+
+
+if menu == 1:
+    print("What income would you like to add")
+elif menu ==2:
+    print("What expense would you like to add")
+elif menu ==3:
+    print("Your amount is ")
+elif menu ==4:
+    print("Goodbye!")
